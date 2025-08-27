97619352b154f329e21efe8d92afbd9f8955514a
hanon-a <lordsnote6418@gmail.com>
2025-08-27 11:49:31 +0900

    user interaction 속성

--- src/html/js/23-script.js
+++ src/html/js/23-script.js
@@ -1,3 +1,6 @@
-document.querySelector('.flip-button').addEventListener('click', () => {
-  document.querySelector('.card').classList.toggle('is-flipped');
+const flip = document.querySelector('.flip-button');
+const card = document.querySelector('.card');
+
+flip.addEventListener('click', () => {
+  card.classList.toggle('is-flipped');
 });
